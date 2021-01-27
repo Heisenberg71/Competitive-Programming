e47792c40f67e4a587d897ec79bccf04b1916786
Heisenberg71 <69238722+Heisenberg71@users.noreply.github.com>
2021-01-27 09:52:22 +0600

    Create Upper bound.cpp

---
+++ Upper bound.cpp
@@ -0,0 +1,13 @@
+int bs_upper_bound(int a[], int n, int x) {
+    int l = 0;
+    int h = n; // Not n - 1
+    while (l < h) {
+        int mid = (l + h) / 2;
+        if (x >= a[mid]) {
+            l = mid + 1;
+        } else {
+            h = mid;
+        }
+    }
+    return l;
+}
