211b81ee8fe911d298193437f96823b48cc11e7d
Heisenberg71 <69238722+Heisenberg71@users.noreply.github.com>
2021-02-27 18:04:33 +0600

    Create Articulation Edges.cpp

---
+++ Articulation Edges.cpp
@@ -0,0 +1,26 @@
+#define ll long long  /// MACROS NEEDED
+#define pb push_back  /// MACROS NEEDED
+#define LIM 100005    /// MACROS NEEDED
+ll dis[LIM], vis[LIM], par[LIM], low[LIM], cnt, n;
+vector <ll> ver[LIM];
+vector <pair<ll,ll> > ans;
+void dfs(ll u)
+{
+    dis[u] = low[u] = cnt++;
+    for(ll i = 0; i < ver[u].size(); i++){
+        ll v = ver[u][i];
+        if(v != par[u]){     
+            if(vis[v] == 0){  /// TREE EDGES
+                par[v] = u;   /// SAVING PARTENTS
+                vis[v] = 1;   
+                dfs(v);
+                low[u] = min(low[u], low[v]);
+            }
+            else if(vis[v] == 1){   /// BACK EDGES
+                low[u] = min(low[u], dis[v]);
+            }
+            if(dis[u] < low[v]) ans.pb({min(u,v), max(u,v)});  /// ARTICULATION EDGES
+        }
+    }
+    vis[u] = 2;   /// FORWARD EDGES
+}
