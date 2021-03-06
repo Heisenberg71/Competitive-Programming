a92892bbba5a010291a0341ca239f423498e4ac4
Heisenberg71 <69238722+Heisenberg71@users.noreply.github.com>
2021-03-06 21:11:06 +0600

    Update How many zeroes.cpp

--- How many zeroes.cpp
+++ How many zeroes.cpp
@@ -1,6 +1,7 @@
 ll dp[15][5][5][5], dp2[15][5][5];
 ll f2(ll pos, ll issmall, ll num)
 {
+    /// HOW MANY NUMBERS SMALLER THAN n IS POSSIBLE FROM THIS STATE (pos,issmall)
     if(pos == s[num].size()) return 1;
     if(dp2[pos][issmall][num] != -1) return dp2[pos][issmall][num];
     ll lo = 0, hi = s[num][pos]-'0', sum = 0;
@@ -18,6 +19,8 @@ ll f(ll pos, ll issmall, ll hasstarted, ll num)
     if(issmall) hi = 9;
     for(; lo <= hi; lo++){
         ll tem = f(pos+1, issmall | (lo<hi), hasstarted | (lo != 0), num);
+        /// ALREADY STARTED AND RIGHT-NOW WE ARE PUTTING 0 AT CURRENT POSSITION
+        /// SO WE WILL HAVE TO FIND OUT IN HOW MANY WAYS THIS ZERO WILL CONTRIBUTE
         if(hasstarted and lo == 0) tem += f2(pos+1,issmall | (lo<hi),num);
         sum += tem;
     }
