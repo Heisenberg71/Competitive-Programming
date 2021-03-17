550c1a16c7217d7dd648eedd113e79a7d116c748
Heisenberg71 <69238722+Heisenberg71@users.noreply.github.com>
2021-03-17 17:16:55 +0600

    Update Binary Search.cpp

--- Binary Search.cpp
+++ Binary Search.cpp
@@ -41,24 +41,7 @@ ll bs_upper_bound(ll a[], ll n, ll x) {
 
 /// ARRAY IDX = [0, 1, 2, 3, 4, 5, (6), 7, 8, 9, ...] = [FALSE, FLASE, FALSE, FLASE, FALSE, TRUE, TRUE, TRUE, TRUE, TRUE, ...]
 /// WHEN THE FIRST TRUE INDEX IS TO FIND OUT IN THE ARRAY IN THIS FORMATION FOR HERE WHICH IS INDEX NO. 6
-
 ll spbs1(ll lo, ll hi)
-{
-    ll lo = 0, hi = n-1; /// ELEMENTS OF THE ARRAY MUST CONTAIN FROM [0 TO N-1] INDESES 
-    while(lo+1 < hi){
-        ll md = (lo+hi)/2;
-        if() /// THE CONDITION FOR TRUE
-            lo = md;
-        else
-            hi = md;
-    }
-    return lo;
-}
-
-
-/// ARRAY IDX = [0, 1, 2, 3, 4, 5, (6), 7, 8, 9, ...] = [TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, FALSE, FALSE, FALSE, FALSE, ...]
-/// WHEN THE LAST TRUE INDEX IS TO FIND OUT IN THE ARRAY IN THIS FORMATION FOR HERE WHICH IS INDEX NO. 6
-ll sbps2(ll lo, ll hi)
 {
     ll lo = 0, hi = n-1; /// ELEMENTS OF THE ARRAY MUST CONTAIN FROM [0 TO N-1] INDESES 
     while(lo+1 < hi){
@@ -69,4 +52,21 @@ ll sbps2(ll lo, ll hi)
             lo = md;
     }
     return hi;
+}
+
+
+
+/// ARRAY IDX = [0, 1, 2, 3, 4, 5, (6), 7, 8, 9, ...] = [TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, FALSE, FALSE, FALSE, FALSE, ...]
+/// WHEN THE LAST TRUE INDEX IS TO FIND OUT IN THE ARRAY IN THIS FORMATION FOR HERE WHICH IS INDEX NO. 6
+ll spbs2(ll lo, ll hi)
+{
+    ll lo = 0, hi = n-1; /// ELEMENTS OF THE ARRAY MUST CONTAIN FROM [0 TO N-1] INDESES 
+    while(lo+1 < hi){
+        ll md = (lo+hi)/2;
+        if() /// THE CONDITION FOR TRUE
+            lo = md;
+        else
+            hi = md;
+    }
+    return lo;
 }
