31e2f43b572c3743f9ceff4317c980510134a4c6
Samin Yasir <69238722+Heisenberg71@users.noreply.github.com>
2021-04-08 19:28:04 +0600

    Update Sieve of Eratosthenes.cpp

--- Sieve of Eratosthenes.cpp
+++ Sieve of Eratosthenes.cpp
@@ -1,10 +1,13 @@
-bool status[10000005]; /// NEED A GLOBAL ARRAY
+#define LIM 10000005
+#define pb push_back
+
+bool status[LIM]; /// NEED A GLOBAL ARRAY
 vector <int> v;
 void make_primes(int N)
 {
     int i, j, sqrtN;
     status[0] = 1, status[1] = 1;
-    sqrtN = int( sqrt((double) N )); /// have to check primes up to (sqrt(N))
+    sqrtN = int( sqrt((double) N )) + 1; /// have to check primes up to (sqrt(N))
     for(i = 4; i <= N; i+=2) status[i] = 1;
     for(i = 3; i <= sqrtN; i+= 2){  /// so, i is a prime, so, discard all the multiples
         if(status[i] == 0){
@@ -12,6 +15,6 @@ void make_primes(int N)
                 status[j] = 1;
         }
     }
-    for(i = 0; i < 10000005; i++) if(status[i] == 0) v.pb(i);
+    for(i = 0; i < LIM; i++) if(status[i] == 0) v.pb(i);
     //for(i = 0; i < 10000005; i++) cout << v[i] << endl;
 }
