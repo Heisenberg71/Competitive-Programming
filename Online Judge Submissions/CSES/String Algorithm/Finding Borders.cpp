cb1001bc4a45dd1951c19294747f5a8bf428be91
Samin Yasir <69238722+Heisenberg71@users.noreply.github.com>
2022-08-07 15:50:49 +0600

    Create Finding Borders.cpp

---
+++ Online Judge Submissions/CSES/String Algorithm/Finding Borders.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define nl '\n'
+#define LIM 1000006
+using namespace std;
+
+ll prefHash[2][LIM], basePower[2][LIM];
+ll mod[2] = {1000000007, 1000000009};
+ll base[2] = {31, 101};
+string str;
+char temp[LIM];
+
+void preCal()
+{
+    prefHash[0][0] = prefHash[1][0] = str[0] - 'a' + 1;
+    basePower[0][0] = basePower[1][0] = 1;
+
+   for(int j = 0; j < 2; j++){
+        for(int i = 1; i < str.size(); i++){
+            basePower[j][i] = (basePower[j][i - 1]*base[j]) %mod[j];
+            prefHash[j][i] = (prefHash[j][i - 1]*base[j] + str[i] - 'a' + 1) %mod[j];
+        }
+    }
+}
+
+ll evaluateHash(int idx, int L, int R)
+{
+    if(L == 0) return prefHash[idx][R];
+    return (prefHash[idx][R] - (prefHash[idx][L - 1]*basePower[idx][R - L + 1]) %mod[idx] + mod[idx]) %mod[idx];
+}
+
+int main()
+{
+    scanf("%s", temp);
+    str = temp;
+
+    preCal();
+
+    for(int i = 0, j = str.size() - 1; i + 1 < str.size(); i++, j--){
+        if(evaluateHash(0, 0, i) == evaluateHash(0, j, str.size() - 1) && evaluateHash(1, 0, i) == evaluateHash(1, j, str.size() -1))
+            printf("%d ", i + 1);
+    }
+    printf("\n");
+
+    return 0;
+}
