844df11e3a5894222be7244d121e0ee851d10aa1
Samin Yasir <69238722+Heisenberg71@users.noreply.github.com>
2022-09-18 16:53:04 +0600

    Create C.cpp

---
+++ Online Judge Submissions/ACM ICPC Submissions/SEC IUJPC/C.cpp
@@ -0,0 +1,65 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+
+#define ll long long
+#define ull unsigned long long
+#define pb push_back
+#define pii pair<int, int>
+#define ff first
+#define ss second
+#define nl '\n'
+#define mod 1000000007
+#define inf 1000000009
+#define MAXX 1000000000000015
+#define LIM 300005
+#define eps 1e-9
+#define pi acos(-1)
+
+using namespace std;
+using namespace __gnu_pbds;
+
+#define ordered_set tree<pii, null_type,less<pii>, rb_tree_tag, tree_order_statistics_node_update>
+
+void FAST_IO() { ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0); }
+
+/**
+
+Notes:
+
+
+*/
+
+
+
+int main()
+{
+    ///MUST READ THE POINTS BELOW BEFORE SUBMIT
+
+    FAST_IO();
+
+    ll n, m;
+    cin >> n >> m;
+    bool flag = 0;
+    while(m--) {
+        string s;
+        cin >> s;
+        if(s == "in") n++;
+        else n--;
+        if(n < 4) flag = 1;
+    }
+    if(n < 4) flag = 1;
+    if(flag) cout << "YES\n";
+    else cout << "NO\n";
+
+    return 0;
+    ///MUST READ THE POINTS BELOW BEFORE SUBMIT
+}
+/**
+    1. READ PROBLEM STATEMENT CAREFULLY.
+    2. LOOK SPECIAL CASE (LIKE, N = 1).
+    3. LOOK FOR OVERFLOW.
+    4. LOOK FOR OUT OF BOUNDS.
+    5. DO NOT STUCK WITH A SINGLE APPROACH.
+    6. THINK OUT OF THE BOX.
+**/
