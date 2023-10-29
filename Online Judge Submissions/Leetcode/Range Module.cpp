c2c2f26f4744d5aff08e007bbbfd85a1cccfdeb3
Samin Yasir <69238722+Heisenberg71@users.noreply.github.com>
2023-10-30 01:31:00 +0600

    Update Range Module.cpp

--- Online Judge Submissions/Leetcode/Range Module.cpp
+++ Online Judge Submissions/Leetcode/Range Module.cpp
@@ -1,1 +1,75 @@
-  
+
+class Node{
+    public:
+    int l, r;
+    bool state;
+    Node *left = NULL, *right = NULL;
+    Node(int l, int r, bool st) {
+        this->l = l;
+        this->r = r;
+        this->state = st;
+    }
+};
+
+class RangeModule {
+public:
+    Node *root;
+
+    RangeModule() {
+        root = new Node(0, 1e9, false);
+    }
+
+    bool update(int l, int r, bool state, Node* root) {
+        if(root->l >= l && root->r <= r) {
+            root->state = state;
+            root->left = NULL;
+            root->right = NULL;
+            return state;
+        }
+        if(l > root->r || r < root->l)return root->state;
+
+        int mid = root->l + (root->r - root->l) / 2;
+        if(root->left == NULL) {
+            root->left = new Node(root->l, mid, root->state);
+            root->right = new Node(mid + 1, root->r, root->state);
+        }
+
+        bool left = update(l, r, state, root->left);
+        bool right = update(l, r, state, root->right);
+        root->state = left & right;
+        return root->state;
+    }
+    
+    void addRange(int left, int right) {
+        update(left, right - 1, true, root);
+    }
+
+    bool q(Node *root, int l, int r) {
+        if((root->l > l && root->r < r) || root->left == NULL)return root->state;
+
+        int mid = root->l + (root->r - root->l) / 2;
+        if(r <= mid){
+            return q(root->left, l, r);
+        }else if(l > mid){
+            return q(root->right, l, r);
+        }else{
+            return q(root->left, l, r) && q(root->right, l, r);
+        }
+    }
+    
+    bool queryRange(int left, int right) {
+        return q(root, left, right - 1);
+    }
+    
+    void removeRange(int left, int right) {
+        update(left, right - 1, false, root);
+    }
+};
+
+/**
+ * Your RangeModule object will be instantiated and called as such:
+ * RangeModule* obj = new RangeModule();
+ * obj->addRange(left,right);
+ * bool param_2 = obj->queryRange(left,right);
+ * obj->removeRange(left,right);
+ */
