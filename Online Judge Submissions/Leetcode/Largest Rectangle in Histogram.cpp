af4d6cea302f4cb250e3d6d6e315e1202faca9a7
Samin Yasir <samin99099@gmail.com>
2024-01-23 23:30:07 +0600

    Update Largest Rectangle in Histogram.cpp

--- Online Judge Submissions/Leetcode/Largest Rectangle in Histogram.cpp
+++ Online Judge Submissions/Leetcode/Largest Rectangle in Histogram.cpp
@@ -22,3 +22,53 @@ public:
         return max_area;
     }
 }; 
+..........................................................................................................................
+class Solution {
+private:
+    int findLargestRectangle(int start, int finish, vector<int>& heights){
+        if(start == finish) return heights[start];
+        
+        // cout << start << " " << finish << '\n';
+        
+        int mid = start + (finish - start)/2;
+        int largestRectangleLeftSide = findLargestRectangle(start, mid, heights);
+        int largestRectangleRightSide = findLargestRectangle(mid + 1, finish, heights);
+        int largestRectangle = max(largestRectangleLeftSide, largestRectangleRightSide);
+        
+        int L = mid, R = mid + 1;
+        int minHeight = min(heights[L], heights[R]);
+        largestRectangle = max(largestRectangle, 2*minHeight);
+        
+        while(start < L || R < finish){
+            if(start <= L - 1 && R + 1 <= finish){
+                if(heights[L - 1] < heights[R + 1]){
+                    minHeight = min(minHeight, heights[R + 1]);
+                    R++;
+                    largestRectangle = max(largestRectangle, (R - L + 1)*minHeight);
+                }
+                else{
+                    minHeight = min(minHeight, heights[L - 1]);
+                    L--;
+                    largestRectangle = max(largestRectangle, (R - L + 1)*minHeight);
+                }
+            }
+            else if(start <= L - 1){
+                minHeight = min(minHeight, heights[L - 1]);
+                L--;
+                largestRectangle = max(largestRectangle, (R - L + 1)*minHeight);
+            }
+            else{
+                minHeight = min(minHeight, heights[R + 1]);
+                R++;
+                largestRectangle = max(largestRectangle, (R - L + 1)*minHeight);
+            }
+        }
+        
+        return largestRectangle;
+    }
+public:
+    int largestRectangleArea(vector<int>& heights) {
+        int largestRectangleArea = findLargestRectangle(0, heights.size() - 1, heights);
+        return largestRectangleArea;
+    }
+};
