f4f2be221ec2c9c288855817755091890935e45d
Samin Yasir <samin99099@gmail.com>
2024-03-31 22:36:11 +0600

    Update Longest Valid Parentheses.cpp

--- Online Judge Submissions/Leetcode/Longest Valid Parentheses.cpp
+++ Online Judge Submissions/Leetcode/Longest Valid Parentheses.cpp
@@ -24,3 +24,33 @@ public:
         return longestValidParenthesesLength;
     }
 };
+
+
+// Space complexity: O(1)
+class Solution {
+public:
+    int longestValidParentheses(string s) {
+        int leftCount = 0, rightCount = 0;
+        int longestValidParenthesesLength = 0;
+        for(auto& u: s){
+            if(u == '(') leftCount++;
+            else rightCount++;
+
+            if(leftCount == rightCount) longestValidParenthesesLength = 
+                max(longestValidParenthesesLength, leftCount + rightCount);
+            if(leftCount < rightCount) leftCount = rightCount = 0;
+        }
+
+        leftCount = 0, rightCount = 0;
+        for(int i = s.size() - 1; i >= 0;  i--){
+            if(s[i] == ')') rightCount++;
+            else leftCount++;
+
+            if(leftCount == rightCount) longestValidParenthesesLength = 
+                max(longestValidParenthesesLength, leftCount + rightCount);
+            if(leftCount > rightCount) leftCount = rightCount = 0;
+        }
+
+        return longestValidParenthesesLength;
+    }
+};
