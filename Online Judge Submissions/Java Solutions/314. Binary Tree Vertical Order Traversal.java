b7f33d43be9432b23342c6f6694e90c0cb96ce9b
Samin Yasir <samin99099@gmail.com>
2025-07-11 12:10:56 +0600

    Create 314. Binary Tree Vertical Order Traversal.java

---
+++ Online Judge Submissions/Java Solutions/314. Binary Tree Vertical Order Traversal.java
@@ -0,0 +1,40 @@
+class Solution {
+  public List<List<Integer>> verticalOrder(TreeNode root) {
+    List<List<Integer>> output = new ArrayList();
+    if (root == null) {
+      return output;
+    }
+
+    Map<Integer, ArrayList> columnTable = new HashMap();
+    // Pair of node and its column offset
+    Queue<Pair<TreeNode, Integer>> queue = new ArrayDeque();
+    int column = 0;
+    queue.offer(new Pair(root, column));
+
+    int minColumn = 0, maxColumn = 0;
+
+    while (!queue.isEmpty()) {
+      Pair<TreeNode, Integer> p = queue.poll();
+      root = p.getKey();
+      column = p.getValue();
+
+      if (root != null) {
+        if (!columnTable.containsKey(column)) {
+          columnTable.put(column, new ArrayList<Integer>());
+        }
+        columnTable.get(column).add(root.val);
+        minColumn = Math.min(minColumn, column);
+        maxColumn = Math.max(maxColumn, column);
+
+        queue.offer(new Pair(root.left, column - 1));
+        queue.offer(new Pair(root.right, column + 1));
+      }
+    }
+
+    for(int i = minColumn; i < maxColumn + 1; ++i) {
+      output.add(columnTable.get(i));
+    }
+
+    return output;
+  }
+}
